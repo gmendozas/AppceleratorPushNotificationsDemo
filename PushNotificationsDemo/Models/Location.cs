6bec0bc24328b757f661f94700fa80bc60c807ca
sinnerUniverse <thethruthpower@gmail.com>
2015-10-02 16:41:57 -0500

    Added classes for location support.

---
+++ PushNotificationsDemo/Models/Location.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PushNotificationsDemo.Models
+{
+    public class Location
+    {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string MaxDistance { get; set; }
+    }
+}
---
+++ PushNotificationsDemo/Models/PushNotification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PushNotificationsDemo.Models
+{
+    public class PushNotification
+    {
+        public PushPayload payload { get; set; }
+        public Location location { get; set; }
+    }
+}
